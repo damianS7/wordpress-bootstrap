ef1ada256096d9aa12f989ef0d11525da3043f1d
damianS7 <josepwnz@gmail.com>
2019-11-10 19:17:14 +0100

    El controlador ahora redirecciona a la misma pagina en vez de retornar una vista.

--- wp-content/plugins/xtube/backend/controllers/class.tags-controller.php
+++ wp-content/plugins/xtube/backend/controllers/class.tags-controller.php
@@ -5,12 +5,6 @@ use Xtube\Backend\XtubeBackend;
 use Xtube\Backend\Models\Tag;
 
 class TagsController {
-    public function view_tags() {
-        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-            return $this->render($this->handle_forms());
-        }
-        return $this->render();
-    }
 
     // Metodo para procesar los formularios (POST)
     public function handle_forms() {
@@ -18,24 +12,34 @@ class TagsController {
             $tag_name = sanitize_text_field($_POST['name']);
             $tag_description = sanitize_text_field($_POST['description']);
             if (Tag::add_tag($tag_name, $tag_description)) {
-                return array('success' => 'The tag has been created.');
+                $data['success'] = 'The tag has been created.';
+                set_transient('tags_view_data', $data, 60*60);
             } else {
-                return array('error' => 'Something went wrong, tag not created.');
+                $data['error'] = 'Something went wrong, tag not created.';
+                set_transient('tags_view_data', $data, 60*60);
             }
         }
 
         if (isset($_POST['delete_tag'])) {
             $tag_id = sanitize_text_field($_POST['tag_id']);
             if (Tag::delete_tag($tag_id)) {
-                return array('success' => 'The tag has been deleted.');
+                $data['success'] = 'The tag has been deleted.';
+                set_transient('tags_view_data', $data, 60*60);
             } else {
-                return array('error' => 'Something went wrong, tag not deleted.');
+                $data['error'] = 'Something went wrong, tag not deleted.';
+                set_transient('tags_view_data', $data, 60*60);
             }
+        }
+
+        if (wp_redirect($_SERVER['HTTP_REFERER'])) {
+            exit;
         }
     }
 
     // Metodo para renderizar la vista.
-    public function render($data = array()) {
+    public function render() {
+        $data = get_transient('tags_view_data');
+        delete_transient('tags_view_data');
         $data['tags'] = Tag::get_tags();
         return XtubeBackend::view('tags.php', $data);
     }
