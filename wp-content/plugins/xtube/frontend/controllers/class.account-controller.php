66b4d9b66340f7437d963fa40d3e8fa90b662bd4
damianS7 <josepwnz@gmail.com>
2019-11-11 23:36:24 +0100

    archivos sin uso

--- wp-content/plugins/xtube/frontend/controllers/class.account-controller.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-class PluginName_Frontend_AccountController {
-    public function view_accounts() {
-        $data = array('success_message' => 'view loaded!');
-        return PluginName_Frontend::view('accounts.php', $data);
-    }
-}
--- wp-content/plugins/xtube/frontend/models/class.account-model.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-class PluginName_Frontend_Account {
-    public static function get_account($account_name) {
-        // Query a la base de datos
-    }
-}
