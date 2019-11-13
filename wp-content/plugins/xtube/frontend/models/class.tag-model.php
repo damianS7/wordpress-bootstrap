1de1bc88bc319dcf5958f708d55529810813a3b7
damianS7 <josepwnz@gmail.com>
2019-11-13 20:21:05 +0100

    query para obtener los tags de un video

--- wp-content/plugins/xtube/frontend/models/class.tag-model.php
+++ wp-content/plugins/xtube/frontend/models/class.tag-model.php
@@ -8,12 +8,14 @@ class Tag {
         return $wpdb->get_results($query);
     }
 
-    public static function get_popular_tags() {
+    public static function get_tags_from_video($video_id) {
         global $wpdb;
-        $query = "SELECT name FROM XTB_TAGS 
-        INNER JOIN XTB_VIDEO_TAGS AS tvt
-        ON id = tvt.tag_id
-        LIMIT 10";
+        $query = "SELECT name FROM XTB_TAGS AS t_tags
+        JOIN XTB_VIDEO_TAGS AS t_vtag ON t_tags.id = t_vtag.tag_id
+        WHERE t_vtag.video_id = '{$video_id}'";
         return $wpdb->get_results($query);
+    }
+
+    public static function get_popular_tags() {
     }
 }
