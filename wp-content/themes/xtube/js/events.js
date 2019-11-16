5122deaaf6250290eae86d5154621de4e228abe7
damianS7 <josepwnz@gmail.com>
2019-11-16 12:37:28 +0100

    funcion para reportar

--- wp-content/themes/xtube/js/events.js
+++ wp-content/themes/xtube/js/events.js
@@ -1,4 +1,33 @@
 
+function ajax_post(url, data) {
+    jQuery.ajax({
+        type: "post",
+        url: url,
+        data: data,
+        error: function (response) {
+            console.log(response);
+        },
+        success: function (response) {
+            // Actualiza el mensaje con la respuesta
+            console.log('success:: ' + response);
+        }
+    });
+}
+
+function report() {
+    var video_id = document.vote_form.elements['video_id'].value;
+    var reason = document.report_form.elements['reason'].value;
+
+    var data = {
+        "video_id": video_id,
+        "reason": reason,
+        "url": ajax_var.url,
+        "action": "video_report",
+        "nonce": ajax_var.nonce
+    }
+    ajax_post(ajax_var.url, data);
+}
+
 function vote(v) {
     var video_id = document.vote_form.elements['video_id'].value;
     var data = {
