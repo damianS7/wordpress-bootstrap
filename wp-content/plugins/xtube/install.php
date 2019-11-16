8e63a384acca3fa1fd75a104db2a00b81de36047
damianS7 <josepwnz@gmail.com>
2019-11-16 18:01:29 +0100

    corregidos varios bug y agregado las tablas que faltaban

--- wp-content/plugins/xtube/install.php
+++ wp-content/plugins/xtube/install.php
@@ -11,13 +11,12 @@ class XtubeInstall {
             url VARCHAR(300) NOT NULL UNIQUE,
             iframe VARCHAR(300) NOT NULL, 
             title VARCHAR(255) NOT NULL,
-            description VARCHAR(255) NOT NULL,
             img_src VARCHAR(300) NOT NULL,
             duration VARCHAR(10) NOT NULL DEFAULT(0),
             views INT NOT NULL DEFAULT(0),
             upvotes INT NOT NULL DEFAULT(0),
             downvotes INT NOT NULL DEFAULT(0),
-            posted_at TIMESTAMP NOT NULL,
+            posted_at TIMESTAMP NOT NULL DEFAULT (CURRENT_TIMESTAMP), 
             PRIMARY KEY(id)
         ) ENGINE=InnoDB;';
 
@@ -26,8 +25,8 @@ class XtubeInstall {
             video_id INT NOT NULL,
             tag_id INT NOT NULL,
             PRIMARY KEY(id),
-            FOREIGN KEY(video_id) REFERENCES XTB_VIDEOS(id),
-            FOREIGN KEY(tag_id) REFERENCES XTB_TAGS(id)
+            FOREIGN KEY(video_id) REFERENCES XTB_VIDEOS(id) ON DELETE CASCADE ON UPDATE CASCADE,
+            FOREIGN KEY(tag_id) REFERENCES XTB_TAGS(id) ON DELETE CASCADE ON UPDATE CASCADE
         ) ENGINE=InnoDB;';
         
         $table_settings = 'CREATE TABLE IF NOT EXISTS XTB_SETTINGS (
@@ -43,49 +42,60 @@ class XtubeInstall {
             PRIMARY KEY(id)
         ) ENGINE=InnoDB;';
 
-        $table_comments = 'CREATE TABLE IF NOT EXISTS XTB_COMMENTS (
-            id INT AUTO_INCREMENT NOT NULL,
-            video_id INT NOT NULL,
-            name VARCHAR(50) NOT NULL UNIQUE,
-            comment TEXT,
-            PRIMARY KEY(id),
-            FOREIGN KEY(video_id) REFERENCES XTB_VIDEOS(id)
-        ) ENGINE=InnoDB;';
-
-        $table_reported_comments = 'CREATE TABLE IF NOT EXISTS XTB_REPORTED_COMMENTS (
-            id INT AUTO_INCREMENT NOT NULL,
-            comment_id INT NOT NULL,
-            reason TEXT,
-            PRIMARY KEY(id),
-            FOREIGN KEY(comment_id) REFERENCES XTB_COMMENTS(id)
-        ) ENGINE=InnoDB;';
-
-        $table_reported_videos = 'CREATE TABLE IF NOT EXISTS XTB_REPORTED_VIDEOS (
+        $table_reported_videos = 'CREATE TABLE IF NOT EXISTS XTB_VIDEO_REPORTS (
             id INT AUTO_INCREMENT NOT NULL,
             video_id INT NOT NULL,
             reason TEXT,
             PRIMARY KEY(id),
-            FOREIGN KEY(video_id) REFERENCES XTB_VIDEOS(id)
+            FOREIGN KEY(video_id) REFERENCES XTB_VIDEOS(id) ON DELETE CASCADE ON UPDATE CASCADE
         ) ENGINE=InnoDB;';
 
-        $wpdb->query($table_tags);
+        $table_video_votes_ip = 'CREATE TABLE IF NOT EXISTS XTB_VIDEO_VOTES_IP (
+            id INT AUTO_INCREMENT NOT NULL,
+            video_id INT NOT NULL,
+            ip VARCHAR(20),
+            PRIMARY KEY(id),
+            FOREIGN KEY(video_id) REFERENCES XTB_VIDEOS(id) ON DELETE CASCADE ON UPDATE CASCADE
+        ) ENGINE=InnoDB;';
+
+        $table_video_views_ip = 'CREATE TABLE IF NOT EXISTS XTB_VIDEO_VIEWS_IP (
+            id INT AUTO_INCREMENT NOT NULL,
+            video_id INT NOT NULL,
+            ip VARCHAR(20),
+            PRIMARY KEY(id),
+            FOREIGN KEY(video_id) REFERENCES XTB_VIDEOS(id) ON DELETE CASCADE ON UPDATE CASCADE
+        ) ENGINE=InnoDB;';
+
         $wpdb->query($table_settings);
         $wpdb->query($table_videos);
+        $wpdb->query($table_tags);
         $wpdb->query($table_video_tags);
-        $wpdb->query($table_comments);
-        $wpdb->query($table_reported_comments);
         $wpdb->query($table_reported_videos);
+        $wpdb->query($table_video_votes_ip);
+        $wpdb->query($table_video_views_ip);
+
+        $this->add_data('XTB_SETTINGS', array(
+            'name' => 'videos_per_page',
+            'value' => '8',
+            'description' => 'The numbers of videos that every page can show.'
+        ));
+    }
+
+    public function add_data($table, $data) {
+        global $wpdb;
+        $wpdb->insert($table, $data);
     }
 
     // Desinstalacion
     public function plugin_deactivation() {
         global $wpdb;
-        $wpdb->query('DROP TABLE IF EXISTS XTB_REPORTED_COMMENTS');
-        $wpdb->query('DROP TABLE IF EXISTS XTB_REPORTED_VIDEOS');
-        $wpdb->query('DROP TABLE IF EXISTS XTB_COMMENTS');
-        //$wpdb->query('DROP TABLE IF EXISTS XTB_SETTINGS');
-        $wpdb->query('DROP TABLE IF EXISTS XTB_VIDEOS');
+        $wpdb->query('DROP TABLE IF EXISTS XTB_VIDEO_TAGS');
+        $wpdb->query('DROP TABLE IF EXISTS XTB_VIDEO_REPORTS');
+        $wpdb->query('DROP TABLE IF EXISTS XTB_VIDEO_VOTES_IP');
+        $wpdb->query('DROP TABLE IF EXISTS XTB_VIDEO_VIEWS_IP');
+        $wpdb->query('DROP TABLE IF EXISTS XTB_SETTINGS');
         $wpdb->query('DROP TABLE IF EXISTS XTB_POSTS');
         $wpdb->query('DROP TABLE IF EXISTS XTB_TAGS');
+        $wpdb->query('DROP TABLE IF EXISTS XTB_VIDEOS');
     }
 }
